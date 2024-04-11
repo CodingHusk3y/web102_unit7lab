32c4ef7f265afc7a544b3065ec3b47cedca62cc6
Hieu Cao <ueihhnimoac@gmail.com>
2024-04-11 13:21:47 -0400

    New features

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './CreatePost.css'
+import { supabase } from '../client.js';
 
-const CreatePost = () => {
 
-    const [post, setPost] = useState({title: "", author: "", description: ""})
 
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-        setPost( (prev) => {
-            return {
-                ...prev,
-                [name]:value,
-            }
-        })
-    }
+const createPost = async (event) => {
+    event.preventDefault();
+  
+    await supabase
+      .from('Posts')
+      .insert({title: post.title, author: post.author, description: post.description})
+      .select();
+  
+    window.location = "/";
+  }
 
     return (
         <div>
@@ -30,7 +30,7 @@ const CreatePost = () => {
                 <textarea rows="5" cols="50" id="description" onChange={handleChange}>
                 </textarea>
                 <br/>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onClick={createPost} />
             </form>
         </div>
     )
